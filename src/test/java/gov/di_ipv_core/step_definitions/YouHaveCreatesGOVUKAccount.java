941bf287fed948346ce36193373c6ab2d7f1ead0
gokhakan <hakangok@icloud.com>
2022-03-01 15:29:37 +0000

    You have created GOV.UK account

---
+++ src/test/java/gov/di_ipv_core/step_definitions/YouHaveCreatesGOVUKAccount.java
@@ -0,0 +1,31 @@
+package gov.di_ipv_core.step_definitions;
+
+import gov.di_ipv_core.utilities.BrowserUtils;
+import gov.di_ipv_core.utilities.ConfigurationReader;
+import gov.di_ipv_core.utilities.Driver;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+import java.text.BreakIterator;
+
+public class YouHaveCreatesGOVUKAccount {
+    @Given("user is on You have created GOV UK Account")
+    public void user_is_on_You_have_created_GOV_UK_Account() {
+        Driver.get().get(ConfigurationReader.get("You_have_signed_in_to_GOV.UK_account_URL"));
+        BrowserUtils.waitForPageToLoad(5);
+    }
+
+    @When("user clicks on Continue button")
+    public void user_clicks_on_Continue_button() {
+        System.out.println("tbi");
+    }
+
+    @Then("user is on Enter your details exactly as they appear on your UK passport page")
+    public void user_is_on_Enter_your_details_exactly_as_they_appear_on_your_UK_passport_page() {
+        System.out.println("tbi");
+    }
+
+
+
+}
