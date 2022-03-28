d74376b1632f4df6c8edac8c2b2ccd1246a2970c
gokhakan <hakangok@icloud.com>
2022-03-28 13:39:08 +0100

    resolve conflict

---
+++ src/test/java/gov/di_ipv_core/pages/SupplyDataPage.java
@@ -0,0 +1,18 @@
+package gov.di_ipv_core.pages;
+
+import gov.di_ipv_core.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class SupplyDataPage {
+    public SupplyDataPage(){
+        PageFactory.initElements(Driver.get(), this);
+    }
+
+    @FindBy (id = "jsonPayload")
+    public WebElement supplyDataInJSONFormatBox;
+
+    @FindBy (xpath = "//input[@name='submit']")
+    public WebElement SubmitDataAndGenerateAuthCode;
+}
