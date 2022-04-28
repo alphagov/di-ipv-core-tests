752d21f48858fcc3b91a2a07150e3e73dcc06299
gokhakan <hakangok@icloud.com>
2022-04-28 08:44:07 +0100

    remove wildcard import

--- src/test/java/gov/di_ipv_core/step_definitions/FullJourneyRouteSteps.java
+++ src/test/java/gov/di_ipv_core/step_definitions/FullJourneyRouteSteps.java
@@ -1,6 +1,13 @@
 package gov.di_ipv_core.step_definitions;
 
-import gov.di_ipv_core.pages.*;
+import gov.di_ipv_core.pages.AddressStubPage;
+import gov.di_ipv_core.pages.EnterYourDetailsExactlyPage;
+import gov.di_ipv_core.pages.OrchestratorStubPage;
+import gov.di_ipv_core.pages.YouHaveSignedInToYourGOVUKAccountPage;
+import gov.di_ipv_core.pages.FraudCheckStubPage;
+import gov.di_ipv_core.pages.AnswerSecurityQuestionsPage;
+import gov.di_ipv_core.pages.KnowledgeBasedVerificationStubPage;
+import gov.di_ipv_core.pages.YouHaveSuccessfullyProvedYourIdentityPage;
 import gov.di_ipv_core.utilities.BrowserUtils;
 import gov.di_ipv_core.utilities.Driver;
 import io.cucumber.java.en.Then;
@@ -21,8 +28,9 @@ public class FullJourneyRouteSteps {
         String expectedPageTitle = "Prove your identity - GDS";
         System.out.println("currentPageTitle = " + currentPageTitle);
         System.out.println("expectedPageTitle = " + expectedPageTitle);
-        Assert.assertEquals(expectedPageTitle,currentPageTitle);
+        Assert.assertEquals(expectedPageTitle, currentPageTitle);
     }
+
     @When("I click `Continue`")
     public void i_click_continue() {
         new YouHaveSignedInToYourGOVUKAccountPage().Continue.click();
@@ -61,45 +69,54 @@ public class FullJourneyRouteSteps {
         // Write code here that turns the phrase above into concrete actions
         throw new io.cucumber.java.PendingException();
     }
+
     @Then("I should be on Address \\(Stub)")
     public void i_should_be_on_address_stub() {
         Assert.assertTrue(new AddressStubPage().JSONPayLoader.isDisplayed());
     }
+
     @When("I supply data in JSON format and click `Submit data and generate auth code`")
     public void i_supply_data_in_json_format_and_click_submit_data_and_generate_auth_code() {
         new AddressStubPage().JSONPayLoader.sendKeys("{ \"test\" : \"example\" }");
         new AddressStubPage().SubmitDataAndGenerateAuthCode.click();
         BrowserUtils.waitForPageToLoad(10);
     }
+
     @Then("I should be on Fraud Check \\(Stub)")
     public void i_should_be_on_fraud_check_stub() {
         Assert.assertTrue(new FraudCheckStubPage().JSONPayLoader.isDisplayed());
     }
+
     @When("I supply data in JSON format, Fraud value {int} and click on `Submit data and generate auth code`")
     public void i_supply_data_in_json_format_fraud_value_and_click_on_submit_data_and_generate_auth_code(Integer int1) {
         new FraudCheckStubPage().JSONPayLoader.sendKeys("{ \"test\" : \"example\" }");
         new FraudCheckStubPage().FraudValue.sendKeys("1");
         new FraudCheckStubPage().SubmitDataAndGenerateAuthCode.click();
     }
+
     @Then("I should be on `Answer security questions` page")
     public void i_should_be_on_answer_security_questions_page() {
         Assert.assertTrue(new AnswerSecurityQuestionsPage().Start.isDisplayed());
     }
+
     @When("I click `Start`")
     public void i_click_start() {
         new AnswerSecurityQuestionsPage().Start.click();
         BrowserUtils.waitForPageToLoad(10);
     }
+
     @Then("I should be on Knowledge Based Verification \\(Stub)")
     public void i_should_be_on_knowledge_based_verification_stub() {
         Assert.assertTrue(new KnowledgeBasedVerificationStubPage().JSONPayLoader.isDisplayed());
     }
+
     @When("I supply data in JSON format, Verification value {int} and click on `Submit data and generate auth code`")
     public void i_supply_data_in_json_format_verification_value_and_click_on_submit_data_and_generate_auth_code(Integer int1) {
         new KnowledgeBasedVerificationStubPage().JSONPayLoader.sendKeys("{ \"test\" : \"example\" }");
         new KnowledgeBasedVerificationStubPage().Verification.sendKeys("1");
         new KnowledgeBasedVerificationStubPage().SubmitDataAndGenerateAuthCode.click();
     }
+
     @Then("I should be on You've successfully proved your identity page")
     public void i_should_be_on_you_ve_successfully_proved_your_identity_page() {
         Assert.assertTrue(new YouHaveSuccessfullyProvedYourIdentityPage().Continue.isDisplayed());
